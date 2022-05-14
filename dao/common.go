068feb0581cca636c7b3ba00065e7f55476f37e8
lkh <zxc05020519@163.com>
2022-05-14 21:39:22 +0800

    实现基本用户注册登录及信息功能

--- dao/common.go
+++ dao/common.go
@@ -24,7 +24,7 @@ type Comment struct {
 
 type User struct {
 	Id            int64  `json:"id,omitempty" gorm:"primaryKey"`
-	Name          string `json:"name,omitempty" gorm:"type:varchar(32)"`
+	Name          string `json:"name,omitempty" gorm:"type:varchar(32); index"`
 	Pwd           string `json:",omitempty" gorm:"type:varchar(32)"`
 	FollowCount   int64  `json:"follow_count,omitempty"`
 	FollowerCount int64  `json:"follower_count,omitempty"`
