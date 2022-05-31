5cd89ebdb71dea8de7de39687c666b4213fdc8f7
lkh <zxc05020519@163.com>
2022-05-31 21:35:38 +0800

    重排user和video结构体，更好地对齐内存，节省内存空间

--- dao/common.go
+++ dao/common.go
@@ -3,15 +3,16 @@ package dao
 import "time"
 
 type Video struct {
-	Id            int64     `json:"id,omitempty"`
-	CreatedAt     time.Time `json:"-" gorm:"index"`
+	IsFavorite    bool      `json:"is_favorite,omitempty" gorm:"-"`
+	Id            int64     `json:"id,omitempty" gorm:"primaryKey"`
 	AuthorId      int64     `json:"-"`
-	Author        User      `json:"author"`
-	PlayUrl       string    `json:"play_url,omitempty" gorm:"type:varchar(100)"`
-	CoverUrl      string    `json:"cover_url,omitempty" gorm:"type:varchar(100)"`
 	FavoriteCount int64     `json:"favorite_count,omitempty"`
 	CommentCount  int64     `json:"comment_count,omitempty"`
-	IsFavorite    bool      `json:"is_favorite,omitempty"`
+	PlayUrl       string    `json:"play_url,omitempty" gorm:"type:varchar(100)"`
+	CoverUrl      string    `json:"cover_url,omitempty" gorm:"type:varchar(100)"`
+	CreatedAt     time.Time `json:"-" gorm:"index"`
+
+	Author User `json:"author"`
 }
 
 type Comment struct {
@@ -23,11 +24,13 @@ type Comment struct {
 
 type User struct {
 	// json屏蔽id、密码、随机盐字段
-	Id            int64  `json:"-" gorm:"primaryKey"`
-	Name          string `json:"name,omitempty" gorm:"type:varchar(32); index"`
-	Pwd           string `json:"-" gorm:"type:char(60)"`
-	Salt          string `json:"-" gorm:"type:char(4)"`
-	FollowCount   int64  `json:"follow_count,omitempty"`
-	FollowerCount int64  `json:"follower_count,omitempty"`
-	IsFollow      bool   `json:"is_follow,omitempty"`
+	IsFollow       bool   `json:"is_follow,omitempty" gorm:"-"`
+	Id             int64  `json:"-" gorm:"primaryKey"`
+	FollowCount    int64  `json:"follow_count,omitempty"`
+	FollowerCount  int64  `json:"follower_count,omitempty"`
+	TotalFavorited int64  `json:"total_favorited,omitempty"`
+	FavoriteCount  int64  `json:"favorite_count,omitempty"`
+	Salt           string `json:"-" gorm:"type:char(4)"`
+	Name           string `json:"name,omitempty" gorm:"type:varchar(32); index"`
+	Pwd            string `json:"-" gorm:"type:char(60)"`
 }
